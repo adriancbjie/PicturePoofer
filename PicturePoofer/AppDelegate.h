0bd51295da8ddae836857794fe81c6c318b99deb
Adrian Cheng Bing Jie <adriancbjie@gmail.com>
2012-02-08 11:24:25 +0800

    started project template

---
+++ PicturePoofer/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  PicturePoofer
+//
+//  Created by Adrian Cheng Bing Jie on 8/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ PicturePoofer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PicturePoofer
+//
+//  Created by Adrian Cheng Bing Jie on 8/2/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
